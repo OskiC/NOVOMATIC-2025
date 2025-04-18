a7e97aea28936b4cc37e792e66cf852749bddd6b
OskiC <xoskarx1@gmail.com>
2025-04-19 01:50:09 +0200

    add getter to points in triangle, add font

--- task3/entities/triangle.h
+++ task3/entities/triangle.h
@@ -16,6 +16,10 @@ public:
     Triangle(sf::Uint8 r, sf::Uint8 g, sf::Uint8 b, const std::array<vec2, 3> &spawn_point);
     void draw(sf::RenderTarget &target);
     void move(sf::Keyboard::Key key, sf::Time dt);
+
+    std::array<vec2, 3> getPointsPlace() const {
+        return pointsPlace;
+    }
 };
 
 } // oc
