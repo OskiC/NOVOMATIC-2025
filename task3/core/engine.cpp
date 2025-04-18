15b91e41573d23f7b9b34d18325e02ef4ca4d9ce
OskiC <xoskarx1@gmail.com>
2025-04-18 21:58:57 +0200

    initialize 2 triangles and draw them

--- task3/core/engine.cpp
+++ task3/core/engine.cpp
@@ -5,16 +5,33 @@
 #include "engine.h"
 
 namespace oc {
-    Engine::Engine() {}
+    Engine::Engine() {
+        //initialize 2 triangles
+        constexpr std::array<vec2, 3> spawn1 = {
+            vec2{0, WINDOW_HEIGHT-10},
+            vec2{200, WINDOW_HEIGHT-10},
+            vec2{100, WINDOW_HEIGHT-150}
+        };
+        constexpr std::array<vec2, 3> spawn2 = {
+            vec2{WINDOW_WIDTH, WINDOW_HEIGHT-10},
+            vec2{WINDOW_WIDTH-200, WINDOW_HEIGHT-10},
+            vec2{WINDOW_WIDTH-100, WINDOW_HEIGHT-150}
+        };
+        triangles.push_back(Triangle(255,255,255, spawn1));
+        triangles.push_back(Triangle(120,120,120, spawn2));
+    }
 
     void Engine::reDraw(sf::RenderWindow &window) {
         window.clear();
+        for (auto& triangle : triangles) {
+            triangle.draw(window);
+        }
         window.display();
     }
 
 
     void Engine::run() {
-        sf::RenderWindow window(sf::VideoMode(800, 600), "Novomatic Task 3",
+        sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Novomatic Task 3",
                                 sf::Style::Titlebar | sf::Style::Close);
 
         while (window.isOpen()) {
--- task3/core/engine.h
+++ task3/core/engine.h
@@ -1,13 +1,16 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include "../entities/triangle.h"
 #include "SFML/Graphics.hpp"
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
 
 namespace oc {
 
     class Engine {
         void reDraw(sf::RenderWindow &window);
-
+        std::vector<Triangle> triangles;
 
     public:
         Engine();
