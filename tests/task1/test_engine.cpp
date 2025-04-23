1e7af20c4e33896a15793dd241b26c24c5d1b191
OskiC <xoskarx1@gmail.com>
2025-04-23 22:59:18 +0200

    add LoadFromFile test for task1

--- tests/task1/test_engine.cpp
+++ tests/task1/test_engine.cpp
@@ -1,24 +1,31 @@
 //
 // Created by xoska on 23.04.2025.
 //
+#define UNIT_TEST
 #include <gtest/gtest.h>
 #include "engine.h"
 
 class EngineTest : public ::testing::Test {
 protected:
+    std::string filename;
+    oc::Engine engine;
+
+    EngineTest() : filename("test_file.txt"), engine(filename){}
+
     void SetUp() override {
-        filename = "test_file.txt";
         std::ofstream file(filename);
         file << "query1\nquery2\nquery3\nquery1\n";
         file.close();
-
-        engine = oc::Engine(filename);
+        engine.load_from_file(filename);
     }
 
     void TearDown() override {
         std::remove(filename.c_str());
     }
 
-    std::string filename;
-    oc::Engine engine;
 };
+
+TEST_F(EngineTest, LoadFromFileSuccess) {
+    EXPECT_EQ(engine.queries_list["query1"], 2);
+    EXPECT_EQ(engine.queries_list["query2"], 1);
+}
