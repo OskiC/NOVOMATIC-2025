fda7f2950579c2457521fedb708e1125dccc8326
OskiC <xoskarx1@gmail.com>
2025-04-23 17:06:40 +0200

    create task1

---
+++ task1/engine.cpp
@@ -0,0 +1,8 @@
+//
+// Created by xoska on 23.04.2025.
+//
+
+#include "engine.h"
+
+namespace oc {
+} // oc
---
+++ task1/engine.h
@@ -0,0 +1,16 @@
+//
+// Created by xoska on 23.04.2025.
+//
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+namespace oc {
+
+class Engine {
+
+};
+
+} // oc
+
+#endif //ENGINE_H
