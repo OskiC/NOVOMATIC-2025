8a7c29589befd9ea5c0274ac3cb1868ed25e527f
OskiC <xoskarx1@gmail.com>
2025-04-23 18:42:12 +0200

    implement skeleton for "search engine", create choice_parser, formatting results, run, addQuery

--- task1/engine.cpp
+++ task1/engine.cpp
@@ -5,4 +5,71 @@
 #include "engine.h"
 
 namespace oc {
+    Engine::Engine(const std::string& filename) {
+        if (load_from_file(filename)) {
+
+        }
+        else {
+            isSaved = false;
+        }
+    }
+
+    bool Engine::load_from_file(const std::string& filename) {
+
+    }
+
+    void Engine::addQuery(const std::string& query) {
+        queries_list[query]++;
+    }
+
+    std::vector<std::string> Engine::ask(std::string asked_query) {
+        std::vector<std::string> result;
+
+
+        return result;
+    }
+
+    void Engine::choice_parser(const std::string& question) {
+        if (question == "0") {
+            exit(0);
+        }
+
+        if (question.starts_with("> add:")) {
+            auto n = question.find(':');
+            std::string query = question.substr(n + 2);
+            addQuery(query);
+        }
+
+        if (question.starts_with("> ask:")) {
+            auto n = question.find(':');
+            std::string query = question.substr(n + 2);
+            std::vector<std::string> results = ask(query);
+            format_results(results);
+        }
+
+        else {
+            std::cout << "Wrong command";
+        }
+    }
+
+    void Engine::format_results(const std::vector<std::string>& results) {
+        for (const auto& s : results) {
+            std::cout << "result: " << s << "\n";
+        }
+    }
+
+
+    [[noreturn]] void Engine::run() {
+        std::string choice;
+        while (true) {
+            std::cout << "to add to list write: \"> add: QUERY\"\n";
+            std::cout << "to get result write: \"> ask: QUERY\"\n";
+            std::cout << "to leave write: \"0\"\n";
+
+            std::getline(std::cin, choice);
+
+            choice_parser(choice);
+        }
+    }
+
 } // oc
--- task1/engine.h
+++ task1/engine.h
@@ -5,11 +5,30 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <fstream>
+
 namespace oc {
 
-class Engine {
+    class Engine {
+    private:
+        std::unordered_map<std::string, int> queries_list;
+        bool isSaved;
 
-};
+        void addQuery(const std::string& query);
+        std::vector<std::string> ask(std::string asked_query);
+
+        bool load_from_file(const std::string& filename);
+        void save_to_file(const std::string& filename);
+
+        void choice_parser(const std::string& question);
+        static void format_results(const std::vector<std::string>& results);
+    public:
+        Engine(const std::string& filename);
+        void run();
+    };
 
 } // oc
 
