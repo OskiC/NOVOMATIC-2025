6e957b3fce5967020db0036c1c6322db25f45d5f
OskiC <xoskarx1@gmail.com>
2025-04-23 14:49:17 +0200

    add tests to task3: BinaryOp for int, string, double, char, float

---
+++ tests/task3/test_binaryop.cpp
@@ -0,0 +1,45 @@
+//
+// Created by xoska on 22.04.2025.
+//
+#include <gtest/gtest.h>
+#include "binary_op.h"
+
+class CalculateTest : public ::testing::Test {
+protected:
+    void SetUp() override {}
+};
+
+TEST_F(CalculateTest, BinaryOpInt) {
+    BinaryOp<int> op;
+    Wrapper<int> a(5), b(3);
+    Wrapper<int> result = op(a,b);
+    EXPECT_EQ(result.value, 8);
+}
+
+TEST_F(CalculateTest, BinaryOpString) {
+    BinaryOp<std::string> op;
+    Wrapper<std::string> a("hej"), b("ho");
+    Wrapper<std::string> result = op(a,b);
+    EXPECT_EQ(result.value, "hejho");
+}
+
+TEST_F(CalculateTest, BinaryOpDouble) {
+    BinaryOp<double> op;
+    Wrapper<double> a(2.3), b(1.2);
+    Wrapper<double> result = op(a, b);
+    EXPECT_EQ(result.value, 3.5);
+}
+
+TEST_F(CalculateTest, BinayOpChar) {
+    BinaryOp<char> op;
+    Wrapper<char> a('a'), b('b');
+    Wrapper<char> result = op(a, b);
+    EXPECT_NE(result.value, 'a' + 'b');
+}
+
+TEST_F(CalculateTest, BinaryOpFloat) {
+    BinaryOp<float> op;
+    Wrapper<float> a(1.25f), b(1.24f);
+    Wrapper<float> result = op(a,b);
+    EXPECT_EQ(result.value, 2.49f);
+}
